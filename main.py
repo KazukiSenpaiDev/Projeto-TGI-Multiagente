52d466f5200795257c000f0e8eb828599058a4ae
KazukiSenpaiDev <pedro.seguette@outlook.com>
2026-08-15 22:27:43 -0300

    Criação de uma interface basica usando streamlit

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import streamlit as st
 
-print('Projeto TGI Multiagente')
+
+def enviar_prompt():
+    st.session_state.prompt_enviado = prompt
+    st.session_state.prompt_text_area = ""
+    
+
+st.html("<style>textarea { resize: none !important; }</style>")
+
+
+st.title("Projeto TGI - Multiagente!!!")
+
+
+prompt = st.text_area("Digite seu prompt:", height=200, key="prompt_text_area")
+
+if st.button("Gerar projeto", on_click=enviar_prompt):
+    if st.session_state.prompt_enviado == "":
+        st.warning("Por favor, digite um prompt antes de enviar.")
+    else:
+        st.success("Prompt enviado com sucesso!")
+        st.write("Prompt enviado:", st.session_state.prompt_enviado)
+
